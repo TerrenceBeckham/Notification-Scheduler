831d6a615b4086a9c2c879b55ad11145f6b78d6c
Terrence Beckham <https://github.com/TerrenceBeckham>
2019-04-08 12:27:45 +0200

    Wired up the RadioGroup and RadioGroup buttons. Made jobSchedular and JobInfo object.

--- app/src/main/java/com/afrodroid/notificationscheduler/MainActivity.java
+++ app/src/main/java/com/afrodroid/notificationscheduler/MainActivity.java
@@ -1,10 +1,17 @@
 package com.afrodroid.notificationscheduler;
 
+import android.app.job.JobInfo;
+import android.app.job.JobScheduler;
+import android.content.ComponentName;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private JobScheduler mScheduler;
+    public static final int JOB_ID = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,8 +20,51 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void scheduleJob(View view) {
+        RadioGroup networkOptions = findViewById(R.id.networkOptions);
+        int selectedNetworkID = networkOptions.getCheckedRadioButtonId();
+        int selectedNetworkOption = JobInfo.NETWORK_TYPE_NONE;
+
+        switch (selectedNetworkID) {
+            case R.id.noNetwork:
+                selectedNetworkOption = JobInfo.NETWORK_TYPE_NONE;
+                break;
+            case R.id.anyNetwork:
+                selectedNetworkOption = JobInfo.NETWORK_TYPE_ANY;
+                break;
+            case R.id.wifiNetwork:
+                selectedNetworkOption = JobInfo.NETWORK_TYPE_UNMETERED;
+                break;
+        }
+
+        //Make JobInfo.Builder object used to associate the
+        ComponentName serviceName = new ComponentName(getPackageName(),
+                NotificationJobService.class.getName());
+        JobInfo.Builder builder = new JobInfo.Builder(JOB_ID, serviceName);
+        builder.setRequiredNetworkType(selectedNetworkOption);
+        mScheduler = (JobScheduler) getSystemService(JOB_SCHEDULER_SERVICE);
+
+        //Set constraint(required)
+        boolean constraintSet = selectedNetworkOption != JobInfo.NETWORK_TYPE_NONE;
+        if (constraintSet) {
+            JobInfo myJobInfo = builder.build();
+            mScheduler.schedule(myJobInfo);
+            Toast.makeText(this, getString(R.string.job_has_been_scheduled) + getString(R.string.when_the_contraints_are_met), Toast.LENGTH_SHORT).show();
+
+        } else {
+            Toast.makeText(this, "Please set at least one constraint", Toast.LENGTH_SHORT).show();
+        }
+
+
     }
 
     public void cancelJobs(View view) {
+        //Check to see if the JobScheduler object is null
+        if (mScheduler != null) {
+            mScheduler.cancelAll();
+            mScheduler = null;
+            Toast.makeText(this, getString(R.string.all_jobs_cancelled), Toast.LENGTH_SHORT).show();
+
+        }
+
     }
 }
