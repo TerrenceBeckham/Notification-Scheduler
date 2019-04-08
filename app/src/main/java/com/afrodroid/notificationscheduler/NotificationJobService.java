b96b144ddd746eef421d1b20aeda8df6b01da297
Terrence Beckham <https://github.com/TerrenceBeckham>
2019-04-08 09:13:39 +0200

    Sub-classed JObService and added new service to the manifest

---
+++ app/src/main/java/com/afrodroid/notificationscheduler/NotificationJobService.java
@@ -0,0 +1,16 @@
+package com.afrodroid.notificationscheduler;
+
+import android.app.job.JobParameters;
+import android.app.job.JobService;
+
+public class NotificationJobService extends JobService {
+    @Override
+    public boolean onStartJob(JobParameters params) {
+        return false;
+    }
+
+    @Override
+    public boolean onStopJob(JobParameters params) {
+        return false;
+    }
+}
